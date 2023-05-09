cb737131298b0dce99c4a87a331e945359f6ee55
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-08 23:28:20 -0700

    My first pycodestyle python file

---
+++ beauty.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Person():
+    name = ""
+    age = 12
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def getAge(self):
+        return self.age
+
+    def setAge(self, ageNew):
+        self.age = ageNew
+
+
+Mark = Person("Mark")
+
+Mark.setAge(30)
+
+print(Mark.getAge(), Mark.name)
